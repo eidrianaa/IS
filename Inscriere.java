a936cead525954582bcdf6b62a1439ede9b51cff
eidriana <147625357+eidrianaa@users.noreply.github.com>
2024-04-25 13:46:25 +0300

    Add files via upload

---
+++ Inscriere.java
@@ -0,0 +1,15 @@
+public class Inscriere {
+
+	private int notePrimite;
+
+	public void getMediaLaZi() {
+		// TODO - implement Inscriere.getMediaLaZi
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNotaFinala() {
+		// TODO - implement Inscriere.getNotaFinala
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+	private int nume;
+	private int adresa;
+	private int nrTelefon;
+	private int IDstudent;
+	private int mediaNotelor;
+
+	public void esteEligibilInscriere() {
+		// TODO - implement Student.esteEligibilInscriere
+		throw new UnsupportedOperationException();
+	}
+
+	public void getSeminariiAlese() {
+		// TODO - implement Student.getSeminariiAlese
+		throw new UnsupportedOperationException();
+	}
+
+}
